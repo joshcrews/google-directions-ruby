4e5869c0ba409ed623af3488be0975530b844698
Laurent Arnoud <laurent@spkdev.net>
2011-08-01 16:49:49 +0200

    Update end_location > lat from updated mock
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- test/unit/google_directions_test.rb
+++ test/unit/google_directions_test.rb
@@ -12,7 +12,8 @@ class GoogleDirectionsTest < Test::Unit::TestCase
     assert_equal(4, directions.distance_in_miles)
     assert_equal(6, directions.drive_time_in_minutes)
     assert_equal("http://maps.google.com/maps/api/directions/xml?sensor=false&origin=121+Gordonsville+Highway,+37030&destination=499+Gordonsville+Highway,+38563", directions.xml_call)
-    assert_not_nil(directions.xml =~ /36\.1773300/)
+    # end_location > lat
+    assert_not_nil(directions.xml =~ /36\.1772300/)
   end
   
   def test_directions_not_found
